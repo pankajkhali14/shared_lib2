c2eebdb1be2f5b1c966e9a5dc668108e13ec681f
Pankaj Khali <PankajKh@hcl.com>
2019-01-09 13:45:57 +0530

    add

---
+++ vars/mavenStep.groovy
@@ -0,0 +1,13 @@
+
+def call(body){
+    def config = [:];
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+	body.delegate = config
+    body()
+
+    def maven = tool "${MAVEN}";
+    print maven;
+    stage("${config.name}"){
+        sh "${maven}/bin/mvn ${config.action}"
+    }
+}
